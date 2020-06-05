a0bc68501da1f6ce2312ffe64fc2a3c93afbfb71
Micaela Verucchi <micaela.verucchi@unimore.it>
2020-06-05 16:29:40 +0200

    Work on more files. Need to add pre-post processing
    Signed-off-by: Micaela Verucchi <micaela.verucchi@unimore.it>

--- demo/main.cpp
+++ demo/main.cpp
@@ -3,12 +3,40 @@
 #include "readData.h"
 #include "trackingMetrics.h"
 
+#include <sys/types.h>
+#include <dirent.h>
+
+
+void readDirectory(const std::string& name, std::vector<std::string>& v)
+{
+    DIR* dirp = opendir(name.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != NULL) {
+        std::string filename = dp->d_name;
+        if(filename != "." && filename != "..")
+            v.push_back(filename);
+    }
+    closedir(dirp);
+}
+
 int main(int argc, char *argv[]) {
     bool world = false;
     float threshold = 0.5;
-    auto gt = readMOTFormat("../data/gt.txt",',', true);
-    auto det = readMOTFormat("../data/MOT17-11-FRCNN.txt", ' ');
 
-    computeTrackingMetrics(gt, det, threshold, false);
+    std::string det_folder = "../data/dets/";
+    std::string gt_folder = "../data/gt/";
+
+    std::vector<std::string> files;
+    readDirectory(det_folder, files);
+
+    for(const auto& f:files){
+        std::string gt_test_folder = f.substr(0, f.find(".txt"));
+        std::cout<<gt_test_folder<<std::endl;
+        auto gt = readMOTFormat(gt_folder + gt_test_folder + "/gt/gt.txt",',', true);
+        auto det = readMOTFormat(det_folder + f, ' ');
+        computeTrackingMetrics(gt, det, threshold, false);
+    }
+
+    return EXIT_SUCCESS;
 
 }
